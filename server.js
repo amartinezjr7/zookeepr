57ba105eb2d6c54880a11552c8f81b83f2d704fd
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-02-25 00:45:10 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 const {animals} = require('./data/animals.json');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //parse incoming string or array data
