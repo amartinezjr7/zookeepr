cf7029ad32faa245dcbea01688082265148fa0d8
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-02-25 19:41:36 -0800

    deleted obsolete code

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const {animals} = require('./data/animals.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSOM data
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
-
-
-
 
 
 
